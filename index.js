230733f955762acdedd181aff622a9150fb26a62
easyui <gzhuyangjun@gmail.com>
2017-12-26 19:55:02 +0800

    npm

---
+++ index.js
@@ -0,0 +1,8 @@
+/**
+ * react-native-ezswiper
+ * @author Zhu yangjun<zhuyangjun@gmail.com>
+ * @url https://github.com/easyui/react-native-ezsidemenu
+ */
+
+import EZSideMenu from './src/EZSideMenu'
+module.exports = EZSideMenu
